fcdaca0552d11af9d6965e463123dfb366867de7
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2021-08-07 22:40:24 +0530

    Setup Socket at Client Side

--- public/Assets/js/app.js
+++ public/Assets/js/app.js
@@ -1,6 +1,14 @@
 let MyApp = (function () {
   function init(uid, mid) {
-    alert("From " + uid + " to " + mid);
+    event_process_for_signaling_server();
+  }
+  var socket = null;
+  function event_process_for_signaling_server() {
+    //console.log("event_process_for_signaling_server");
+    socket = io.connect();
+    socket.on("connect", () => {
+      alert("socket connected to client side");
+    });
   }
 
   return {
--- server.js
+++ server.js
@@ -5,9 +5,11 @@ let server = app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port %d", server.address().port);
 });
 
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, {
+  allowEIO3: true, // false by default
+});
 app.use(express.static(path.join(__dirname, "")));
 
 io.on("connection", (socket) => {
-  console.log("socket id is " + socket.id);
+  console.log("your unique socket id is: " + socket.id);
 });
