a7aa891204554b38c96c08f196c26584fe49f070
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2021-08-08 14:08:30 +0530

    Add users and set connection

--- public/Assets/js/app.js
+++ public/Assets/js/app.js
@@ -1,3 +1,33 @@
+let AppProcess = function () {
+  let iceConfiguration = {
+    iceServers: [
+      {
+        url: "stun:stun.l.google.com:19302",
+      },
+      {
+        url: "turn:turn.bistri.com:80",
+        credential: "homeo",
+        username: "homeo",
+      },
+      {
+        url: "turn:turn.anyfirewall.com:443?transport=tcp",
+        credential: "webrtc",
+        username: "webrtc",
+      },
+    ],
+  };
+
+  function setConnection(connId) {
+    let connection = new RTCPeerConnection(iceConfiguration);
+  }
+
+  return {
+    setNewConnection: async function (connId) {
+      await setConnection(connId);
+    },
+  };
+};
+
 let MyApp = (function () {
   var socket = null;
   var user_id = "";
@@ -25,7 +55,21 @@ let MyApp = (function () {
       //console.log("inform_others_about_me");
       //console.log(data);
       addUser(data.other_user_id, data.connId);
+      AppProcess.setNewConnection(data.connId);
     });
+  }
+
+  function addUser(other_user_id, connId) {
+    //console.log("addUser");
+    //console.log(other_user_id);
+    //console.log(connId);
+    var newDivId = $("#otherTemplate").clone();
+    newDivId = newDivId.attr("id", "connId").addClass("other");
+    newDivId.find("h2").text(other_user_id);
+    newDivId.find("video").attr("id", `v_${connId}`);
+    newDivId.find("audio").attr("id", `a_${connId}`);
+    newDivId.show();
+    $("#divUsers  ").append(newDivId);
   }
 
   return {
