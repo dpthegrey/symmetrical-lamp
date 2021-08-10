5f51cbfe6b3a4c47c0921a13574646a52143da0b
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2021-08-10 19:23:39 +0530

    Set Offer Process

--- public/Assets/js/app.js
+++ public/Assets/js/app.js
@@ -1,4 +1,13 @@
 let AppProcess = function () {
+  let peers_connection_ids = []; // Array of connection ids of peers
+  let peers_connection = []; // Array of connections of peers
+
+  let serverProcess;
+  // We are taking SDP_function & my_connId from MyApp on socket connection
+  function _init(SDP_function, my_connId) {
+    serverProcess = SDP_function;
+    my_connection_id = my_connId;
+  }
   let iceConfiguration = {
     iceServers: [
       {
@@ -19,11 +28,54 @@ let AppProcess = function () {
 
   function setConnection(connId) {
     let connection = new RTCPeerConnection(iceConfiguration);
+
+    connection.onnegotiationneeded = async function (event) {
+      await setOffer(connId);
+    };
+    connection.onicecandidate = function (event) {
+      if (event.candidate) {
+        serverProcess(
+          JSON.stringify({
+            icecandidate: event.candidate,
+          }),
+          connId
+        );
+      }
+    };
+    connection.ontrack = function (event) {
+      console.log("on track", event);
+    };
+    peers_connection_ids[connId] = connId; // Add connection id to array
+    peers_connection[connId] = connection; // Add connection to array
+  }
+
+  function setOffer(connId) {
+    // Get connection from array and store it in the variable
+    let connection = peers_connection[connId];
+    // Create offer
+    let offer = await connection.createOffer();
+    // Set local description using created offer
+    await connection.setLocalDescription(offer);
+    // Send offer to peer
+    serverProcess(
+      JSON.stringify(
+        {
+          // sending offer property with value of local
+          // description to other users
+          // in order to identify my connection
+          offer: connection.localDescription,
+        },
+        connId
+      )
+    );
   }
 
   return {
     setNewConnection: async function (connId) {
       await setConnection(connId);
+    },
+    init: async function (SDP_function, my_connId) {
+      await _init(SDP_function, my_connId);
     },
   };
 };
@@ -40,8 +92,18 @@ let MyApp = (function () {
   function event_process_for_signaling_server() {
     //console.log("event_process_for_signaling_server");
     socket = io.connect();
+
+    let SDP_function = function (data, to_connId) {
+      // this will go to server.js on the event of SDPProcess
+      socket.emit("SDPProcess", {
+        message: data,
+        to_connId: to_connId,
+      });
+    };
     socket.on("connect", () => {
+      console.log("socket connected");
       if (socket.connected) {
+        AppProcess.init(SDP_function, socket.id);
         if (user_id != "" && meeting_id != "") {
           socket.emit("userconnect", {
             displayName: user_id,
